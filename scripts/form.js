96795daf4ce262fb568ce38a638b032836aca695
edbaez-dev <edu94baez@gmail.com>
2026-08-06 11:01:42 -0300

    w05 product review form

--- scripts/form.js
+++ scripts/form.js
@@ -1,27 +1,27 @@
 const products = [
   {
     id: "fc-1888",
-    name: "Flux capacitor",
+    name: "flux capacitor",
     averagerating: 4.5,
   },
   {
     id: "fc-2050",
-    name: "Power laces",
+    name: "power laces",
     averagerating: 4.7,
   },
   {
     id: "fs-1987",
-    name: "Time circuits",
+    name: "time circuits",
     averagerating: 3.5,
   },
   {
     id: "ac-2000",
-    name: "Low voltage reactor",
+    name: "low voltage reactor",
     averagerating: 3.9,
   },
   {
     id: "jj-1969",
-    name: "Warp equalizer",
+    name: "warp equalizer",
     averagerating: 5.0,
   }
 ];
